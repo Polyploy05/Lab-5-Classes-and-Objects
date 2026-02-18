aedc23c024bb966b49bbd3bb1714fbace764e856
Polyploy05 <jakada005@gmail.com>
2026-02-17 22:12:05 -0800

    Update Question.py

--- Question.py
+++ Question.py
@@ -2,10 +2,7 @@
 Names: Jacob Miranda & Daniel Puerto
 Date: 2/17/2626
 Group: 7
-Description: The program should read in a file of state capitals, then quiz the 
-user on the capitals of 10 randomly selected states. The user should be given 4 possible 
-answers for each question, and the program should keep track of the user's score. The main file 
-should loop 10 times, asking a question each time, and then record and display the user's final score at the end.
+Description: 
 '''
 import random
 
@@ -57,6 +54,7 @@ class Question:
         for key, value in self._selections.items():
             output += f"{value}. {key}\n"
         return output
+
 
 
 
