441a3b8c14ab132975931ee30d16b33bf6b43863
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-02-17 20:43:15 -0800

    Add files via upload

---
+++ CECS277Lab5main.py
@@ -0,0 +1,68 @@
+'''
+Names:
+Date:
+Group:
+Description:
+'''
+
+
+
+from question import Question
+
+def read_file_to_dictionary(statecapitals.txt):
+    
+    states = {}
+    with open(statecapitals, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                state,capital = line.split(",")
+                state[state] = capital
+    return states
+
+
+def get_user_choice(valid_options):
+    
+    choice = input("Enter choice: "). upper()
+
+    while choice not in valid_options:
+        print(f"Invalid input. Enter {valid_options[0]} - {valid_options[-1]}.")
+        choice = input("Enter choice: ").upper()
+
+    return choice
+
+
+def ask_questions(number, states):
+
+    print(f"{number}.", end="")
+
+    q = Question(states)
+    print(q)
+
+    user_choice = get_user_choice(q.possible_choices)
+
+    if q.check_correct(user_choice):
+        print(q.correct_response())
+        return 1
+    else:
+        print(q.incorrect_response())
+        return 0 
+    
+
+def main():
+    print("Welcome to the State Capitals Quiz")
+
+    states = read_file_to_dictionary("statecapitals.txt")
+
+    score = 0
+    for i in range(1,11):
+        print()
+        score += ask_questions(i, states)
+
+
+    print("\nQuiz complete")
+    print(f"You scored {score} out of 10.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Question.py
@@ -0,0 +1,37 @@
+'''
+Names:
+Date:
+Group:
+Description:
+'''
+
+
+
+import random
+
+class Question:
+    def __init__ (self,state):
+
+
+        pass
+
+
+
+
+
+
+
+
+    def possible_choices(self):
+        return self._possible_choices
+    
+    def correct_response(self):
+        pass
+
+
+
+    def __str__(self):
+
+
+        pass
+
